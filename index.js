809a5faa62d0024c02de88b2c896a6b27b6bfd5b
Debabrata Basak <debabratabasak061@gmail.com>
2024-10-25 18:30:13 +0530

    added the hosted netlify address

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 dbConnection();
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["https://moengage-client.netlify.app/","https://moengage-client.netlify.app","http://localhost:5173", "http://localhost:3000"],
     methods: ["GET", "POST", "PATCH", "DELETE","PUT"],
     credentials: true,
   })
